51a34de66ee8f82981ebed5a679543e0575193b6
BeejLuig <bjcantlupe@gmail.com>
2017-08-02 10:01:27 -0400

    add template file

---
+++ exercises.rb
@@ -0,0 +1,21 @@
+# 1. Create a method that takes a string as an argument and adds the phrase "Only in America!" to the end of it
+
+
+# 2. Create a method to find the maximum value in an array of numbers. For instance: [100,10,-1000] should return 100. Do not use Ruby's built-in .max method.
+
+
+
+# 3. Create a method that takes two arrays as arguments. You can assume both arrays are the same length. The first array will have symbols, the second will have strings. Use a loop to combine each symbol and string and add them to a hash. Return the hash.
+    # Input:
+    # [:toyota, :tesla]
+    # ["Prius", "Model S"]
+
+    # Expected output:
+    # {toyota: "Prius", tesla: "Model S"}
+
+
+
+# 4. Write a method that prints numbers from 1 to 100, except:
+# for multiples of three print "Fizz" instead of the number
+# for multiples of five print "Buzz"
+# Print "FizzBuzz" for numbers that are multiples of both 3 and 5.
